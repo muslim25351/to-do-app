958ebd7ed4dce7cc3c9a5adf7feb77801b996857
abuzer <abuzerj12@gmail.com>
2025-12-31 19:37:31 +0300

    edit db

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "to-do app";
+$host = "sql312.infinityfree.com";
+$user = "if0_40800658";
+$pass = "XReiQu20tp6";
+$db   = "if0_40800658_mytodo";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
